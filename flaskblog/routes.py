05eb767aa45808de81961101c0c9da123affac4e
knichols <kyleanicholson@gmail.com>
2022-09-17 14:19:27 -0700

    Pagination

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -14,7 +14,8 @@ from flask_login import login_user, current_user, logout_user, login_required
 @app.route("/home")
 
 def home():
-    posts = Post.query.all()
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=3)
     return render_template('home.html', posts=posts)
 
 @app.route("/about")
@@ -103,7 +104,7 @@ def new_post():
         flash('Your post has been created!', 'success')
         return redirect(url_for('home'))
     return render_template('create_post.html', title='New Post', form=form, 
-                            legend='Update Post')
+                            legend='New Post')
 
 @app.route("/post/<int:post_id>")
 def post(post_id):
@@ -139,3 +140,13 @@ def delete_post(post_id):
     db.session.commit()
     flash("Your post has been deleted!", 'success')
     return redirect(url_for('home'))
+
+@app.route("/user/<string:username>")
+
+def user_posts(username):
+    page = request.args.get('page', 1, type=int)
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = Post.query.filter_by(author=user)\
+        .order_by(Post.date_posted.desc())\
+        .paginate(page=page, per_page=3)
+    return render_template('user_posts.html', posts=posts, user=user)
